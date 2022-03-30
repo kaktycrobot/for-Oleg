e4a86c0da676653c1f212c06233c5fe76796483f
Sergey <cthulhudark@gmail.com>
2022-03-30 14:37:20 +0300

    changed the shift function

--- 04_Functions/src/main/kotlin/Main.kt
+++ 04_Functions/src/main/kotlin/Main.kt
@@ -4,24 +4,27 @@ fun main() {
     val secondPart = result.substring(startIndex = result.length / 2)
     println(decryptionOne(firstPart)+"\n"+decryptionTwo(secondPart))
 }
-fun decryptionOne(firstPart: String) : String {
+fun decryptionOne(firstPart: String): String {
 //    var shiftSource: String = firstPart
-    val operationOne = shift(1, firstPart)
+    val operationOne = shift(firstPart, 1)
     val operationTwo = operationOne.replace('5','s')
     val operationThree = operationTwo.replace('4', 'u')
-    val operationFour = shift(-3, operationThree)
+    val operationFour = shift(operationThree, -3)
     val operationFive = operationFour.replace('0', 'o')
     return operationFive
 }
-fun decryptionTwo(secondPart: String) : String {
+fun decryptionTwo(secondPart: String): String {
 //    var shiftSource: String = secondPart
     val operationOne = secondPart.reversed()
-    val operationTwo = shift(-4, operationOne)
+    val operationTwo = shift(operationOne, -4)
     val operationThree = operationTwo.replace('_', ' ')
     return operationThree
 }
 
-fun shift(x: Int, shiftSource: String) : String {
-    val shiftedString = shiftSource.map {char -> char + x}.joinToString("")
-    return shiftedString
+fun shift(shiftSource: String,
+          shift: Int,
+          shiftMethod: ((shiftSource: String, shift: Int) -> String)? = null): String {
+    shiftMethod?.invoke(shiftSource,shift)
+return shiftSource.map {Char -> Char + shift}.joinToString("")
 }
+
